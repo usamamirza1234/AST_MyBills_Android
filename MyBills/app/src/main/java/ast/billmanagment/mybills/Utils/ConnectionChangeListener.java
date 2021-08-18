5271f94988a5f7f66e62a82ef816ca6f59f88ce2
Usama <usamak.mirza@gmail.com>
2021-08-18 18:16:37 +0500

    init Base Project Android

---
+++ MyBills/app/src/main/java/ast/billmanagment/mybills/Utils/ConnectionChangeListener.java
@@ -0,0 +1,5 @@
+package ast.billmanagment.mybills.Utils;
+
+public interface ConnectionChangeListener   {
+    void onConnectionChanged(boolean isConnectionAvailable);
+}
---
+++ MyBills/app/src/main/java/ast/billmanagment/mybills/Utils/CustomAlertConfirmationInterface.java
@@ -0,0 +1,9 @@
+package ast.billmanagment.mybills.Utils;
+
+public interface CustomAlertConfirmationInterface {
+    public abstract void callConfirmationDialogPositive();
+
+
+    public abstract void callConfirmationDialogNegative();
+
+}
